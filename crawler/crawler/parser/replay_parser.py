3542a7095b9867548f9b766d02df96860981480e
Rowan <github@rh.codes>
2023-01-10 18:23:26 +0000

    Create parser for body, still need to figure out arg_type '189'

--- crawler/crawler/parser/replay_parser.py
+++ crawler/crawler/parser/replay_parser.py
@@ -1,5 +1,6 @@
 import struct
 import datetime
+from enum import Enum
 
 
 class Header:
@@ -28,9 +29,7 @@ class Header:
         self.num_time_code = num_time_code
         self.unknown1 = unknown1
         self.str_filename = str_filename.decode('ascii')
-
         self.date_time = date_time  # Not sure what to even do with this? Seems to be slightly wrong?
-
         self.str_version = str_version.decode('ascii')
         self.str_build_date = str_build_date.decode('ascii')
         self.version_minor = version_minor
@@ -69,7 +68,7 @@ def find_double_zero_termination(data: bytes) -> (bytes, list):
     return zero_termed_string, new_data_obj
 
 
-def parse_header(replay_bytes: bytes) -> (Header, int):
+def parse_header(replay_bytes: bytes) -> (Header, bytes):
     """ Parse the header part of the replay file.
 
     https://docs.python.org/3/library/struct.html#format-characters
@@ -91,7 +90,7 @@ def parse_header(replay_bytes: bytes) -> (Header, int):
 
     Returns:
         Header object containing some info about the battle
-        Int containing the last number of bytes left in the replay that represent the body.
+        bytes containing the remaining bytes in the replay that represent the body.
 
     """
     # char             str_magic[6];    6 bytes
@@ -160,10 +159,100 @@ def parse_header(replay_bytes: bytes) -> (Header, int):
     )
     created_header = Header(**unpacked)
 
-    return created_header, len(replay_bytes[18:])
+    return created_header, replay_bytes[18:]
 
 
 def parse_replay_file(data: bytes) -> Header:
     header, pointer = parse_header(data)
     return header
 
+
+class Chunk:
+    def __init__(self, timecode: int = None, chunkcode: int = None, number: int = None, number_of_args: int = None, args: list = None):
+        self.timecode: int = timecode
+        self.chunkcode: int = chunkcode
+        self.number: int = number
+        self.number_of_args: int = number_of_args
+        self.args: list = args
+
+    def as_dict(self):
+        return self.__dict__
+
+
+class ArgType(Enum):
+    """Enum arg types."""
+    Integer = 0  # int32 4 bytes
+    Float = 1  # float32 4 bytes
+    Boolean = 2  # bool 1 byte
+    ObjectId = 3  # uint32 4 bytes
+    four = 4  # 4 bytes, no idea what it is.
+    five = 5  # 0 bytes? No idea again.
+    Position = 6  # float32 3 * 12 bytes [x,y,z]
+    ScreenPosition = 7  # int32 2 * 8 bytes
+    eight = 8  # 16 bytes, no idea what it is.
+
+
+def read_bytes(arg_type: ArgType, data: bytes):
+    if arg_type == ArgType.Boolean:
+        return_value = struct.unpack("<B", data[:1])
+        data = data[1:]
+    elif arg_type in (ArgType.Integer, ArgType.Float, ArgType.ObjectId, ArgType.four):
+        return_value = struct.unpack("<I", data[:4])
+        data = data[4:]
+    elif arg_type == ArgType.ScreenPosition:  # Two floats.
+        return_value = struct.unpack("<II", data[:8])
+        data = data[8:]
+    elif arg_type == ArgType.Position:  # Three floats.
+        return_value = struct.unpack("<III", data[:12])
+        data = data[12:]
+    elif arg_type == ArgType.eight:  # 16 bytes.
+        return_value = struct.unpack("<IIII", data[:16])
+        data = data[16:]
+    else:
+        return_value = None
+
+    return return_value, data
+
+
+def parse_body(data: bytes):
+    win = False
+    cache = []
+    while data:
+        unpacked = header2dict(
+            names=["timecode", "chunkcode", "number", "number_of_args"],
+            struct_format="<IIIB",
+            data=data[:13],
+        )
+        c = Chunk(**unpacked)
+        data = data[13:]
+        argcount = {}
+
+        for _ in range(unpacked["number_of_args"]):
+            arg_type, count = struct.unpack("<BB", data[:2])
+            # print(data[:15])
+            # print(arg_type, count)
+            argcount[ArgType(arg_type)] = count
+            data = data[2:]
+
+        chunk_args = {}
+
+        for num, (arg_type, count) in enumerate(argcount.items()):
+            arg_values = []
+
+            for each in range(count):
+                value, data = read_bytes(arg_type, data)
+                arg_values.append(value[0])
+
+            chunk_args[f"arg_{num}"] = {arg_type.name: arg_values}
+
+        c.args = chunk_args
+
+        if c.chunkcode == 1093:
+            print(c.as_dict())
+
+
+with open('your_replay_file.rep', "rb") as f:
+    stream = f.read()
+    header, body = parse_header(stream)
+    print(header.as_dict())
+    parse_body(body)
